e3729aec5c7136b69f89399b259f0d6cb41e40cd
Belinda Li <26785408+belindal@users.noreply.github.com>
2019-03-30 16:24:16 -0700

    script changes

--- training_percent_labels_script.py
+++ training_percent_labels_script.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import os
 import json
+from typing import Any, Dict, Tuple
 
 from allennlp.commands.subcommand import Subcommand
 from allennlp.common.checks import check_for_gpu
@@ -20,35 +21,12 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 results_dir = "percent_labels_experiment"
 
-
 def train_model(params: Params,
                 serialization_dir: str,
+                results_fn: str,
                 file_friendly_logging: bool = False,
                 recover: bool = False,
-                force: bool = False) -> Model:
-    """
-    Trains the model specified in the given :class:`Params` object, using the data and training
-    parameters also specified in that object, and saves the results in ``serialization_dir``.
-    Parameters
-    ----------
-    params : ``Params``
-        A parameter object specifying an AllenNLP Experiment.
-    serialization_dir : ``str``
-        The directory in which to save results and logs.
-    file_friendly_logging : ``bool``, optional (default=False)
-        If ``True``, we add newlines to tqdm output, even on an interactive terminal, and we slow
-        down tqdm's output to only once every 10 seconds.
-    recover : ``bool``, optional (default=False)
-        If ``True``, we will try to recover a training run from an existing serialization
-        directory.  This is only intended for use when something actually crashed during the middle
-        of a run.  For continuing training a model on new data, see the ``fine-tune`` command.
-    force : ``bool``, optional (default=False)
-        If ``True``, we will overwrite the serialization directory if it already exists.
-    Returns
-    -------
-    best_model: ``Model``
-        The model with the best epoch weights.
-    """
+                force: bool = False) -> Tuple[Model, Dict[str, Any]]:
     prepare_environment(params)
     create_serialization_dir(params, serialization_dir, recover, force)
     stdout_handler = prepare_global_logging(serialization_dir, file_friendly_logging)
@@ -56,43 +34,45 @@ def train_model(params: Params,
     cuda_device = params.params.get('trainer').get('cuda_device', -1)
     check_for_gpu(cuda_device)
 
+    params.to_file(os.path.join(serialization_dir, CONFIG_NAME))
+
+    evaluate_on_test = params.pop_bool("evaluate_on_test", False)
+
     trainer_type = params.get("trainer", {}).get("type", "default")
 
-    all_f1s = []
-    all_ps = []
-    all_rs = []
-    for percent in range(1, 100, 20):  # [20, 40, 60, 80]
-        Params.params["dataset_reader"]["partial_label_threshold"] = float(percent) * 0.01
-        if trainer_type == "default":
-            # Special logic to instantiate backward-compatible trainer.
-            pieces = TrainerPieces.from_params(params, serialization_dir, recover)  # pylint: disable=no-member
-            trainer = Trainer.from_params(
-                    model=pieces.model,
-                    serialization_dir=serialization_dir,
-                    iterator=pieces.iterator,
-                    train_data=pieces.train_dataset,
-                    validation_data=pieces.validation_dataset,
-                    params=pieces.params,
-                    validation_iterator=pieces.validation_iterator)
-            evaluation_iterator = pieces.validation_iterator or pieces.iterator
-            evaluation_dataset = pieces.test_dataset
+    if trainer_type == "default":
+        # Special logic to instantiate backward-compatible trainer.
+        pieces = TrainerPieces.from_params(params, serialization_dir, recover)  # pylint: disable=no-member
+        trainer = Trainer.from_params(
+                model=pieces.model,
+                serialization_dir=serialization_dir,
+                iterator=pieces.iterator,
+                train_data=pieces.train_dataset,
+                validation_data=pieces.validation_dataset,
+                params=pieces.params,
+                validation_iterator=pieces.validation_iterator)
+        evaluation_iterator = pieces.validation_iterator or pieces.iterator
+        evaluation_dataset = pieces.test_dataset
 
-        else:
-            trainer = TrainerBase.from_params(params, serialization_dir, recover)
-            # TODO(joelgrus): handle evaluation in the general case
-            evaluation_iterator = evaluation_dataset = None
-        print("Training " + str(percent) + "% labelled:")
-        try:
-            metrics = trainer.train()
-        except KeyboardInterrupt:
-            # if we have completed an epoch, try to create a model archive.
-            if os.path.exists(os.path.join(serialization_dir, _DEFAULT_WEIGHTS)):
-                logging.info("Training interrupted by the user. Attempting to create "
-                             "a model archive using the current best epoch weights.")
-                archive_model(serialization_dir, files_to_archive=params.files_to_archive)
-            raise
+    else:
+        trainer = TrainerBase.from_params(params, serialization_dir, recover)
+        # TODO(joelgrus): handle evaluation in the general case
+        evaluation_iterator = evaluation_dataset = None
 
-        # Evaluate
+    params.assert_empty('base train command')
+
+    try:
+        metrics = trainer.train()
+    except KeyboardInterrupt:
+        # if we have completed an epoch, try to create a model archive.
+        if os.path.exists(os.path.join(serialization_dir, _DEFAULT_WEIGHTS)):
+            logging.info("Training interrupted by the user. Attempting to create "
+                         "a model archive using the current best epoch weights.")
+            archive_model(serialization_dir, files_to_archive=params.files_to_archive)
+        raise
+
+    # Evaluate
+    if evaluation_dataset and evaluate_on_test:
         logger.info("The model will be evaluated using the best epoch weights.")
         test_metrics = evaluate(trainer.model, evaluation_dataset, evaluation_iterator,
                                 cuda_device=trainer._cuda_devices[0], # pylint: disable=protected-access,
@@ -102,11 +82,30 @@ def train_model(params: Params,
         for key, value in test_metrics.items():
             metrics["test_" + key] = value
 
-        cleanup_global_logging(stdout_handler)
+    elif evaluation_dataset:
+        logger.info("To evaluate on the test set after training, pass the "
+                    "'evaluate_on_test' flag, or use the 'allennlp evaluate' command.")
 
-        # Now tar up results
-        archive_model(serialization_dir, files_to_archive=params.files_to_archive)
-        dump_metrics(os.path.join(results_dir, "metrics.json"), metrics, log=True)
+    cleanup_global_logging(stdout_handler)
+
+    # Now tar up results
+    archive_model(serialization_dir, files_to_archive=params.files_to_archive)
+    dump_metrics(os.path.join(results_dir, results_fn), metrics, log=True)
+
+    # We count on the trainer to have the model with best weights
+    return trainer.model, metrics
+
+
+def main():
+    serialization_dir = "temp"
+
+    params = Params.from_file("./training_config/coref_label_experiments.jsonnet", "")
+    all_f1s = []
+    all_ps = []
+    all_rs = []
+    for percent in range(1, 100, 20):  # [20, 40, 60, 80]
+        params.params["trainer"]["active_learning"]["percent_label_experiments"]["percent_labels"] = percent * 0.01
+        best_model, metrics = train_model(params, serialization_dir, str(percent) + ".json")
         print("  Best Epoch = " + str(metrics['best_epoch']) + " with F1 = " + str(metrics['best_validation_coref_f1']))
         all_f1s.append(metrics['best_validation_coref_f1'])
         all_ps.append(metrics['best_validation_coref_precision'])
@@ -114,15 +113,7 @@ def train_model(params: Params,
 
     with open(results_dir + "/all_f1s.json", 'w') as outfile:
         json.dump({"F1": all_f1s, "Precision": all_ps, "Recall": all_rs}, outfile)
-
-
-def main():
-    serialization_dir = "temp"
-    params = Params.from_file("./training_config/coref_label_experiments.jsonnet", "")
-    train_model(params, serialization_dir)
     pdb.set_trace()
-
-
 
 
 if __name__ == '__main__':
